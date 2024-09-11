ceb73bbc3a242bec1e2bcacd415ebade619dd9e0
Vishal Shekha <vishal.tshekha@gmail.com>
2024-09-12 02:01:21 +0530

    edits

--- src/App.js
+++ src/App.js
@@ -5,12 +5,25 @@ import Footer from "./Components/Footer"
 import Navbar from "./Components/Navbar"
 import Login from "./Pages/Login"
 import Sports from "./Pages/Sports"
+import SignUp from "./Pages/SignUp";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './custom-theme.css';
+
+
 function App() {
   return (
     
     <div>
     <Navbar/>
-    <Login/>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-12 col-sm-8 col-md-6 col-lg-4">
+          <Login/>
+          <SignUp/>
+        </div>
+      </div>
+    </div>
     <Sports/>
     <Footer/>
     </div>
